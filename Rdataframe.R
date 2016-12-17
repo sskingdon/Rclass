36400978c1aec52e01c9bb2459ade9f852442a6d
sskingdon <b00505028@ntu.edu.tw>
2016-12-17 16:03:45 +0800

    2016/12/17 16:03

---
+++ Rdataframe.R
@@ -0,0 +1,10 @@
+#R翻轉教室  2016/12/17 15:25 Start
+#01-RBasic-06-List-DataFrame
+#隨筆
+
+# ` 符號的按鍵，在美式鍵盤中位於1的左方，這個符號唸作：Grave accent，是R在console中的跳脫字元。
+#data.frame的各個元素必須是以下幾種類型之一：數值(numeric)、字串(character)、布林(logical)、類別(factor)、數值矩陣(numeric
+#matrix)、 list或data.frame。
+
+
+
